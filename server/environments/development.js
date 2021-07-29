a15f19a9e3c42eda72ce1a526ab5c8c5faaf347f
Khadija Al-Khashab <khadija.alkhashab@Khadija.local>
2021-07-30 00:11:45 +0300

    edit port value

--- server/environments/development.js
+++ server/environments/development.js
@@ -2,7 +2,7 @@
 
 exports.ENV = 'development';
 exports.DEBUGGING = true;
-exports.SERVER_PORT = "3000";
+exports.SERVER_PORT = process.env.PORT || "3000";
 exports.MONGO_DB_CONNECTION = "mongodb+srv://smart_sequencer_user:user@123@cluster0.ohafu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 exports.MONGO_DB_NAME = "";
 exports.SECRET_KEY = "XFLExAydVjo+ZPu3wNio9LrrUlQu2ILYfPsAPEHTWytyhTGHeGXp/KR9IEELzIOUWOYJnY6JXz4GgKlf0vTl25cQeggre51cWEoOr5WEEDe5TKNSnqsS0wVfYRovwKM+PYlKyN3vzokRUknXb9GxKFuFIxqviJ+3ddOdI/SDyIDD/wOkI+wFy+4g2/d20UU8rHc0XIeyZ0WOhTcjSeLi8FL4TsDO2tXL8cNEEWj9C1fdu7VoLdAwdXHuvkBWyZpaYbKsKZmjJa/H4kV03pdDZXNyr5YMvn7Yrdg+0dC6oqXDV0iPWDHTa/CErx9Lzu/OU0XMEl4sJ6pwAJKIM+zh9w==";
